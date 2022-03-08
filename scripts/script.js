62a59f513d3f4c9cf0c9c950acf3492cd87d5348
Andrey Sukhovev <andrei5s@yandex.ru>
2022-03-08 20:57:10 +0300

    Проектная работа4

--- scripts/script.js
+++ scripts/script.js
@@ -2,24 +2,24 @@ const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.profile__info');
 const closePopup = popup.querySelector('.popup__close');
 let edit = document.querySelector('#edit');
-let conteins = edit.classList.contains('popup_opened');
+let conteins = edit.classList.contains('popup__opened');
 const formElement = document.querySelector('.popup__user');
-const nameInput = document.querySelector('.popup_input_name');
-const jobInput = document.querySelector('.popup_input_job');
+const nameInput = document.querySelector('.popup__input-name');
+const jobInput = document.querySelector('.popup__input-job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 console.log(conteins);
 
 openPopup.addEventListener('click', function() {
-    popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup__opened');
 });
 
 closePopup.addEventListener('click', function() {
-    popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup__opened');
 });
 
 function togglePopup() {
-    popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup__opened');
 }
 
 function formSubmitHandler(evt) {
