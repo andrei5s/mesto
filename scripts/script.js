91f3bcdb26870df8d807dcb18a6759e65a469a40
Andrey Sukhovev <andrei5s@yandex.ru>
2022-03-11 15:36:35 +0300

    Проектная работа4

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,34 @@
 const popup = document.querySelector('.popup');
-const openPopup = document.querySelector('.profile__edit');
-const closePopup = popup.querySelector('.popup__close');
-let edit = document.querySelector('#edit');
+const openPopupButton = document.querySelector('.profile__edit');
+const closePopupButton = popup.querySelector('.popup__close');
+
 const formElement = document.querySelector('.popup__user');
 const nameInput = document.querySelector('#inputname');
 const jobInput = document.querySelector('#inputjob');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
-function togglePopup() {
-    edit.classList.add('popup_opened');
-    popup.classList.toggle('popup_opened');
+function openPopup() {
+    popup.classList.add('popup_opened');
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
 }
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    togglePopup();
+    closePopup();
 }
 
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
