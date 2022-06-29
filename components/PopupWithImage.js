cfe6bda360628bc4a9c3e81d134ba57d4454bab2
Andrey Sukhovev <andrei5s@yandex.ru>
2022-06-29 19:43:05 +0300

    Проектная работа 8

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -2,12 +2,13 @@ import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
 
-    open(text, link) {
+    open(name, link) {
         const image = this._popup.querySelector('.popup__img-card');
         const title = this._popup.querySelector('.popup__img-title');
 
+        title.textContent = name;
         image.src = link;
-        title.textcontent = text;
+        image.alt = name;
 
         super.open();
     }
